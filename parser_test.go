9c913c85574cce4ed0679cc394538a67b766331c
Mark Karpeles <magicaltux@gmail.com>
2021-08-16 19:14:00 +0900

    make test an actual test

--- parser_test.go
+++ parser_test.go
@@ -2,7 +2,6 @@ package gsh
 
 import (
 	"io"
-	"log"
 	"strings"
 	"testing"
 )
@@ -10,6 +9,12 @@ import (
 func TestTokens(t *testing.T) {
 	sess := New()
 	p := sess.newParser(strings.NewReader("echo 'This is a\ntoken' and a few more\necho cmd2 ; echo cmd3"), "(test)")
+
+	expect := []string{
+		"echo|This is a\ntoken|and|a|few|more",
+		"echo|cmd2",
+		"echo|cmd3",
+	}
 
 	for {
 		cmd, err := p.readCommand()
@@ -24,6 +29,11 @@ func TestTokens(t *testing.T) {
 		if err != nil {
 			t.Errorf("failed, error in argv: %s", err)
 		}
-		log.Printf("cmd = %q", args)
+
+		v := strings.Join(args, "|")
+		if v != expect[0] {
+			t.Errorf("failed, expected %q but got %q", expect[0], v)
+		}
+		expect = expect[1:]
 	}
 }
